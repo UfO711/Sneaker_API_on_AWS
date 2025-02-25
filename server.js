9ffa9259ccb115be2884ebeba93a120a6199dc51
Chuanhao LI <chuanhaoli@ChuanhaodeMacBook-Pro.local>
2025-02-25 17:21:20 +0800

    delete listen function

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/api/product-prices/:productId', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});     
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });     
